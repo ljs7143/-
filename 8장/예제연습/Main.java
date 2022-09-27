35d185055e00423e0f86ea32f0c45a2a89a49b52
LeeJunSeo <77109803+ljs7143@users.noreply.github.com>
2022-09-27 22:15:40 +0900

    Update Main.java
    add [Anonymouse class]

--- 8장/예제연습/Main.java
+++ 8장/예제연습/Main.java
@@ -3,5 +3,22 @@ public class Main{
 		RemoteControl rc;
 		rc = new Television();
 		rc = new Audio();
+		
+		rc.turnOn();
+		//익명구현객체구현. interface메소드가 재사용될 필요없을 떄 일회용으로 사용하기 위함.
+		//모든 메소드의 구현부를 정의해야함.
+		RemoteControl test = new RemoteControl(){
+		    public void turnOn(){
+		        System.out.println("익명구현객체");
+		    }
+		    public void turnOff(){
+		        System.out.println("익명구현객체2");
+		    }
+		    public void setVolume(int volume){
+		        System.out.println("3");
+		    }
+		};
+		test.turnOn();
+		
 	}
 }
