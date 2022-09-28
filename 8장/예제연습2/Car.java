629e66838f6df2e3a0ac6bab95537efd92d7ae75
LeeJunSeo <77109803+ljs7143@users.noreply.github.com>
2022-09-28 22:35:50 +0900

    Update Car.java
    타이어필드 인터페이스 배열로 관리

--- 8장/예제연습2/Car.java
+++ 8장/예제연습2/Car.java
@@ -1,12 +1,21 @@
 public class Car{
-    Tire frontTire = new HanKookTire();
-    Tire backTire = new KumhoTire();
+    // Tire frontTire = new HanKookTire();
+    // Tire backTire = new KumhoTire();
+    Tire[] tires = {
+        new HanKookTire(),
+        new HanKookTire()
+    };
+    //타이어필드를 인터페이스 배열로 관리해줘도 된다 
+    
+  
+    
     
     public void run(){
-        frontTire.roll();
-        backTire.roll();
+        // frontTire.roll();
+        // backTire.roll();
+        for(Tire tire : tires){
+            tire.roll();
+        }
     }
 }
-    
-    
     
