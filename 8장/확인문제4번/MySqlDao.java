596b82ed86d4f646b83165f433f88f12140a56de
LeeJunSeo <77109803+ljs7143@users.noreply.github.com>
2022-09-29 23:02:10 +0900

    Create MySqlDao.java

---
+++ 8장/확인문제4번/MySqlDao.java
@@ -0,0 +1,17 @@
+public class MySqlDao implements DataAccessObject{
+ 
+    
+    public void select(){
+        System.out.println("MySql에서 검색");
+    }
+    public void insert(){
+        System.out.println("MySql에서 삽입");
+    }
+    public void update(){
+        System.out.println("MySql에서 수정");
+    }
+     
+    public void delete(){
+        System.out.println("MySql에서 삭제");
+    }
+}
