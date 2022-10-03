934086d686d6db13f0c5fb0b611536e56d4127c7
LeeJunSeo <77109803+ljs7143@users.noreply.github.com>
2022-10-03 22:55:28 +0900

    Create Window.java

---
+++ 9장/버튼이벤트익명객체로/Window.java
@@ -0,0 +1,27 @@
+public class Window {
+    Button button1 = new Button();
+    Button button2 = new Button();
+
+    //필드초기값으로 익명객체 대입
+
+    Button.OnClickListener listener = new Button.OnClickListener() {
+        @Override
+        public void onClick(){
+            System.out.println("전화를 겁니다");
+        }
+    };
+
+
+    Window(){
+        button1.SetOnClickListener(listener);
+        button2.SetOnClickListener(new Button.OnClickListener() {
+         
+            @Override
+            public void onClick(){
+                System.out.println("메시지를 보냅니다.");
+            }
+        }
+        );
+
+    }
+}
