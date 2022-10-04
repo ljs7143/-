9b6d47a68526e7c1a2f9a8dba56d74b6ac6e8595
LeeJunSeo <77109803+ljs7143@users.noreply.github.com>
2022-10-04 19:46:55 +0900

    Create Main.java

---
+++ 9장/확인문제4번/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        Car.Tire tire = myCar.new Tire();
+        Car.Engine engine = new Car.Engine();
+    }
+
+}
